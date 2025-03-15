5aae01438925ba4d2bfbd44cf1c75c2e73ba9af5
Martijn Staal <27222398+mastaal@users.noreply.github.com>
2025-03-15 18:17:41 +0100

    Move inwerkingtredingsbepaling label code to utils

--- parlhistnl/management/commands/experiment_inwerkingtredingsbepalingen.py
+++ parlhistnl/management/commands/experiment_inwerkingtredingsbepalingen.py
@@ -12,6 +12,7 @@
     SPDX-License-Identifier: EUPL-1.2
 """
 
+import datetime
 import json
 import logging
 import re
@@ -20,10 +21,14 @@ from typing import Any
 from django.core.management import BaseCommand
 
 from parlhistnl.models import Staatsblad
+from parlhistnl.utils.inwerkingtredingsbepalingen import (
+    find_inwerkingtredingsbepaling,
+    InwerkingtredingsbepalingType,
+)
 
 logger = logging.getLogger(__name__)
 iwt_re: re.Pattern = re.compile(
-    r"(de\s+artikelen\s+van\s+deze\s+(rijks)?wet\s+treden|deze\s+(rijks)?wet\s+treedt)(,\s+onder\s+toepassing\s+van\s+artikel\s+12,\s+eerste lid,\s+van\s+de\s+Wet\s+raadgevend\s+referendum,)?\s+in\s+werking[\w,\s]+.(\s*indien[\w,\s]+.)?",
+    r"(de\s+artikelen\s+van\s+deze\s+(rijks)?wet\s+treden|deze\s+(rijks)?wet\s+treedt)(,\s+met\s+uitzondering\s+van[\w,\s]+,\s*)?(,\s+onder\s+toepassing\s+van\s+artikel\s+12,\s+eerste lid,\s+van\s+de\s+Wet\s+raadgevend\s+referendum,)?\s+in\s+werking[\w,\s]+.(\s*indien[\w,\s]+.)?",
     re.IGNORECASE,
 )
 
@@ -60,32 +65,47 @@ class Command(BaseCommand):
         iwtr_zd_zd = 0
         iwtr_onbekend = 0
 
+        # There are some special cases which never have an inwerkingtredingsbepaling
+        vaststellingswet_grondwetswijziging = 0
+        goedkeuringswet_verdrag = 0
+
         for wet in wetten:
 
-            cleaned_tekst1 = wet.tekst.replace("\xa0", " ")
-            cleaned_tekst = f"{wet.preferred_url}\n{cleaned_tekst1}"
+            # Handle special cases which never contain a inwerkingtredingsbepaling
+            if wet.is_vaststelling_grond_grondwetswijziging():
+                vaststellingswet_grondwetswijziging += 1
+                continue
 
-            matches = list(iwt_re.finditer(cleaned_tekst))
+            if wet.is_goedkeuringswet_verdrag():
+                goedkeuringswet_verdrag += 1
 
-            if len(matches) == 1:
-                print(wet.preferred_url, matches[0].group(0))
-                m = matches[0]
+            check_result = find_inwerkingtredingsbepaling(wet)
 
-                # find if it contains KB
-                matcheskb = list(kb_re.finditer(m.group(0)))
-
-                matchesdif = list(dif_re.finditer(m.group(0)))
-                if len(matcheskb) > 0 and len(matchesdif) > 0:
+            if (
+                check_result["label"] != InwerkingtredingsbepalingType.ONBEKEND
+                and check_result
+                != InwerkingtredingsbepalingType.GEEN_INWERKINGTREDINGSBEPALING
+            ):
+                if (
+                    check_result["label"]
+                    == InwerkingtredingsbepalingType.DELEGATIE_EN_DIFFERENTIATIE
+                ):
                     labels = [
                         "Inwerkingtredingsbepaling met delegatie en differentiatie"
                     ]
                     iwtr_d_d += 1
-                elif len(matcheskb) > 0 and len(matchesdif) == 0:
+                elif (
+                    check_result["label"]
+                    == InwerkingtredingsbepalingType.DELEGATIE_ZONDER_DIFFERENTIATIE
+                ):
                     labels = [
                         "Inwerkgintredingsbepaling met delegatie zonder differentiatie"
                     ]
                     iwtr_d_zd += 1
-                else:
+                elif (
+                    check_result["label"]
+                    == InwerkingtredingsbepalingType.GEEN_DELEGATIE
+                ):
                     labels = ["Inwerkingtredingsbepaling zonder delegatie"]
                     iwtr_zd_zd += 1
 
@@ -96,10 +116,10 @@ class Command(BaseCommand):
                         "to_name": "text",
                         "type": "labels",
                         "value": {
-                            "start": m.start(0),
-                            "end": m.end(0),
+                            "start": check_result["start"],
+                            "end": check_result["end"],
                             "score": 0.25,
-                            "text": m.group(0),
+                            "text": check_result["text"],
                             "labels": labels,
                         },
                     }
@@ -118,19 +138,27 @@ class Command(BaseCommand):
                 {
                     "id": f"stb-{wet.jaargang}-{wet.nummer}",
                     "data": {
-                        "text": cleaned_tekst,
+                        "text": check_result["cleaned_text"],
                         "stb-id": f"stb-{wet.jaargang}-{wet.nummer}",
                     },
                     "predictions": prediction,
                 }
             )
 
-        with open("task.json", "wt", encoding="utf-8") as jsonfile:
+        with open(
+            f"experiment-inwerkingtredingsbepalingen-task-{datetime.datetime.now().strftime('%Y-%m-%d_%H%M')}.json",
+            "wt",
+            encoding="utf-8",
+        ) as jsonfile:
             json.dump(data, jsonfile)
 
         print("Dumped wetten:", len(data))
         print(f"inwerkingtreding met delegatie, met differentiatie: {iwtr_d_d}")
         print(f"inwerkingtreding met delegatie, zonder differentiatie: {iwtr_d_zd}")
         print(f"inwerkingtreding zonder delegatie: {iwtr_zd_zd}")
+        print(f"goedkeuringswet verdrag: {goedkeuringswet_verdrag}")
+        print(
+            f"vaststellingswet grondwetwijziging: {vaststellingswet_grondwetswijziging}"
+        )
         print(f"inwerkingtreding type onbekend: {iwtr_onbekend}")
         print(iwtr_d_d, iwtr_d_zd, iwtr_zd_zd, iwtr_onbekend)
--- parlhistnl/utils/inwerkingtredingsbepalingen.py
+++ parlhistnl/utils/inwerkingtredingsbepalingen.py
@@ -11,10 +11,88 @@
 """
 
 import logging
+import re
+
+from enum import Enum
 
 from parlhistnl.models import Staatsblad
 
 logger = logging.getLogger(__name__)
+iwt_re: re.Pattern = re.compile(
+    r"(de\s+artikelen\s+van\s+deze\s+(rijks)?wet\s+treden|deze\s+(rijks)?wet\s+treedt)(,\s+met\s+uitzondering\s+van[\w,\s]+,\s*)?(,\s+onder\s+toepassing\s+van\s+artikel\s+12,\s+eerste lid,\s+van\s+de\s+Wet\s+raadgevend\s+referendum,)?\s+in\s+werking[\w,\s]+.(\s*indien[\w,\s]+.)?",
+    re.IGNORECASE,
+)
+
+kb_re: re.Pattern = re.compile(
+    r"bij\s+koninklijk\s+besluit\s+te\s+bepalen\s+tijdstip", re.IGNORECASE
+)
+dif_re: re.Pattern = re.compile(
+    r"verschillend\s+kan\s+worden\s+(vast)?gesteld", re.IGNORECASE
+)
+
+
+class InwerkingtredingsbepalingType(Enum):
+    """Enum class for simple 3-way categorisation of inwerkingtredingsbepalingen"""
+
+    DELEGATIE_EN_DIFFERENTIATIE = (
+        "Inwerkingtredingsbepaling met delegatie en differentiatie"
+    )
+    DELEGATIE_ZONDER_DIFFERENTIATIE = (
+        "Inwerkgintredingsbepaling met delegatie zonder differentiatie"
+    )
+    GEEN_DELEGATIE = "Inwerkingtredingsbepaling zonder delegatie"
+    GEEN_INWERKINGTREDINGSBEPALING = "Geen inwerkingtredingsbepaling"
+    ONBEKEND = "Onbekend"
+
+
+def find_inwerkingtredingsbepaling(stb: Staatsblad) -> dict:
+    """
+    Find inwerkingtredingsbepaling in the given stb
+
+    Returns a dict with:
+        cleaned_text (always): the cleaned text of stb that was evaluated
+        label (always): a InwerkingtredingsbepalingType value
+        start, end, text: if an inwerkingtredingsbepaling was found in the text, the start, end and actual text value of it.
+    """
+
+    if stb.is_vaststelling_grond_grondwetswijziging():
+        label = InwerkingtredingsbepalingType.GEEN_INWERKINGTREDINGSBEPALING
+
+    cleaned_text_1 = stb.tekst.replace("\xa0", " ")
+    cleaned_text = f"{stb.preferred_url}\n{cleaned_text_1}"
+
+    result_dict = {"cleaned_text": cleaned_text}
+
+    matches = list(iwt_re.finditer(cleaned_text))
+
+    if len(matches) == 1:
+        m = matches[0]
+
+        # find if it contains KB
+        matcheskb = list(kb_re.finditer(m.group(0)))
+
+        matchesdif = list(dif_re.finditer(m.group(0)))
+
+        if len(matcheskb) > 0 and len(matchesdif) > 0:
+            label = InwerkingtredingsbepalingType.DELEGATIE_EN_DIFFERENTIATIE
+        elif len(matcheskb) > 0 and len(matchesdif) == 0:
+            label = InwerkingtredingsbepalingType.DELEGATIE_ZONDER_DIFFERENTIATIE
+        else:
+            label = InwerkingtredingsbepalingType.GEEN_DELEGATIE
+
+        result_dict["start"] = m.start(0)
+        result_dict["end"] = m.end(0)
+        result_dict["text"] = m.group(0)
+    else:
+        logger.debug(
+            "Multiple matches for inwerkingtredingsbepaling in %s, defaulting to onbekend",
+            stb,
+        )
+        label = InwerkingtredingsbepalingType.ONBEKEND
+
+    result_dict["label"] = label
+
+    return result_dict
 
 
 def find_related_inwerkingtredingskb(stb: Staatsblad) -> set[Staatsblad]:
